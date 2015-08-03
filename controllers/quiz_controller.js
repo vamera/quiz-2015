ec718eb9ac500bb50d11a2653b1ea45085c7ded9
Vanessa Mejias <vane_glasgow@hotmail.com>
2015-08-03 14:17:22 +0200

    Validacion de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,7 +15,7 @@ exports.load = function(req, res, next, quizId) {
 // GET /quizes
 exports.index = function(req, res){
 	models.Quiz.findAll().then(function(quizes) {
-			res.render('quizes/index.ejs', { quizes: quizes});
+			res.render('quizes/index.ejs', { quizes: quizes, errors: []});
 		}
 	).catch(function(error) { next(error);})
 };
@@ -25,29 +25,44 @@ exports.new = function (req, res) {
 	var quiz = models.Quiz.build ( 	// crea objeto quiz
 		{ pregunta: "Pregunta", respuesta: "Respuesta"}
 	);
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
 	var quiz = models.Quiz.build( req.body.quiz );
-
-// guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	}) // Redireccion HTTP (URL relativo) lista de preguntas
+	
+	quiz
+	.validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+				quiz 	// guarda en DB los campos pregunta y respuesta de quiz
+				.save({fields: ["pregunta","respuesta"]})
+				.then(function(){ res.redirect('/quizes')})
+			}			// res.redirect: Redireccion HTTP a lista de preguntas
+		}
+	);
 };
 
 // GET /quizes/:id
 exports.show = function(req, res){
-	res.render('quizes/show', { quiz: req.quiz});
+	res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res){
 	var resultado = 'Incorrecto';
-		if (req.query.respuesta === req.quiz.respuesta) {
-			resultado = 'Correcto';
+	if (req.query.respuesta === req.quiz.respuesta) {
+		resultado = 'Correcto';
+	}
+	res.render(
+		'quizes/answer', 
+		{ quiz: req.quiz, 
+		  respuesta: resultado,
+		  errors: []
 		}
-		res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
+	);
 };
