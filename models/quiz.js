5f8de4679cd945e0123848462d0cd17485cadfa8
Vanessa Mejias <vane_glasgow@hotmail.com>
2015-08-03 08:47:33 +0200

    Crear preguntas

--- models/quiz.js
+++ models/quiz.js
@@ -1,6 +1,6 @@
 // Definicion del modelo de Quiz
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('Quizzes',
+	return sequelize.define('Quiz',
 							{ pregunta: DataTypes.STRING,
 							 respuesta: DataTypes.STRING
 							});
