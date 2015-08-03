15129d75a8d96765566c0ea7c31ee947af374bc6
Vanessa Mejias <vane_glasgow@hotmail.com>
2015-08-03 15:40:06 +0200

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -75,6 +75,14 @@ exports.update =  function(req, res) {
 	);
 };
 
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+	req.quiz.destroy()
+	.then( function() {
+		res.redirect('/quizes');
+	}).catch(function(error){next(error)});
+};
+
 // GET /quizes/:id
 exports.show = function(req, res){
 	res.render('quizes/show', { quiz: req.quiz, errors: []});
