c443176c658535c56a99c1fb009c4112fbc26666
Vanessa Mejias <vane_glasgow@hotmail.com>
2015-08-19 09:09:48 +0200

    Moderacion de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.get('/author', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definicion de rutas de sesion
 router.get('/login', 	sessionController.new);		// formulario login
@@ -33,7 +34,10 @@ router.get('/quizes/:quizId(\\d+)/edit',	sessionController.loginRequired, quizCo
 router.put('/quizes/:quizId(\\d+)',			sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',		sessionController.loginRequired, quizController.destroy);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', 	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', 		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+		   sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
